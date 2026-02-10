8a10e5dc86397354576c1fe582fb63634709c2a2
Md Mahmud Hasan <mahadymahamudh472@gmail.com>
2026-02-10 06:50:22 +0600

    lsp basedpyright unfinished

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,91 @@
+return {
+
+    -- LSP Support
+        {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                -- Use basedpyright instead of pyright for better Django support
+                ensure_installed = { "lua_ls", "basedpyright" },
+                handlers = {
+                    -- Default handler for all servers
+                    function(server_name)
+                        lspconfig[server_name].setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+
+                    -- Specific override for BasedPyright
+                    ["basedpyright"] = function()
+                        lspconfig.basedpyright.setup({
+                            capabilities = capabilities,
+                            settings = {
+                                basedpyright = {
+                                    analysis = {
+                                        -- "basic" is best for Django to avoid "Member unknown" 
+                                        -- errors on dynamic attributes (like .objects)
+                                        typeCheckingMode = "basic",
+                                        autoSearchPaths = true,
+                                        useLibraryCodeForTypes = true,
+                                        diagnosticMode = "openFilesOnly",
+                                    },
+                                },
+                            },
+                        })
+                    end,
+                },
+            })
+        end,
+    },    -- Completion (Keep your existing cmp config, but ensure these are present)
+    { "hrsh7th/cmp-nvim-lsp" },
+{
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip", -- Required for many completion setups
+            "saadparwaiz1/cmp_luasnip",
+        },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(), -- Force trigger completion
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" }, -- This is what gets data from BasedPyright
+                    { name = "luasnip" },
+                }, {
+                    { name = "buffer" },
+                }),
+            })
+        end,
+    },
+}
--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -1,26 +1,5 @@
 return {
 
-        {
-                "zbirenbaum/copilot.lua",
-                cmd = "Copilot",
-                event = "InsertEnter",
-                config = function()
-                        require("copilot").setup({
-                        suggestion = {
-                                enabled = true,
-                                auto_trigger = true,
-                                keymap = {
-                                        accept = "<C-l>",
-                                next = "<M-]>",
-                                prev = "<M-[>",
-                                dismiss = "<C-]>",
-                                },
-                        },
-                        panel = { enabled = false },
-                })
-            end,
-        },
-
         { "nvim-treesitter/nvim-treesitter" },
         { "nvim-lualine/lualine.nvim" },
         { "catppuccin/nvim", name = "catppuccin" },
@@ -54,24 +33,5 @@ return {
                        require("Comment").setup()
                 end
 
-        },
-        {
-                "williamboman/mason.nvim",
-                build = ":MasonUpdate",
-                config = function()
-                        require("mason").setup()
-                end
-        },
-        {
-                "williamboman/mason-lspconfig.nvim",
-                config = function()
-                        require("mason-lspconfig").setup({
-                                ensure_installed = { "lua_ls", "pyright" }, -- auto-install servers
-                        })
-                end
-        },
-
-        -- Completion
-        { "hrsh7th/nvim-cmp" },
-        { "hrsh7th/cmp-nvim-lsp" }
+        }
 }

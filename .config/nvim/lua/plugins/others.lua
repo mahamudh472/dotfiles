38091ba1be9d8372a8326d0629cf6d723940c902
mahamudh472 <mahadymahamudh472@gmail.com>
2026-02-11 07:59:34 +0600

    telescope added for searching

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -60,5 +60,21 @@ return {
                        require("Comment").setup()
                 end
 
-        }
+        },
+        {
+                'nvim-telescope/telescope.nvim',
+                tag = '0.1.8',
+                dependencies = { 'nvim-lua/plenary.nvim' },
+                config = function()
+                        local builtin = require('telescope.builtin')
+                        -- Keymaps for searching
+                        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
+                        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Search Text in Project" })
+                        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Open Buffers" })
+
+                        -- THE ONES YOU ASKED FOR: Searching Symbols (Classes/Functions)
+                        vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = "Find Symbols (Current File)" })
+                        vim.keymap.set('n', '<leader>fw', builtin.lsp_dynamic_workspace_symbols, { desc = "Find Symbols (Workspace/Project)" })
+                end
+        },
 }

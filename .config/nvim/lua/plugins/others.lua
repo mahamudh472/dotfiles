b9800fbf445608d1ca5705a87c9fa9e5f470e9a0
mahamudh472 <mahadymahamudh472@gmail.com>
2026-02-11 07:26:14 +0600

    tree sitter udpated

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -1,6 +1,33 @@
 return {
 
-        { "nvim-treesitter/nvim-treesitter" },
+{
+        "nvim-treesitter/nvim-treesitter",
+                branch = "master", -- Stay on stable
+                build = ":TSUpdate",
+                dependencies = {
+                        { "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" }, -- Force master here too
+                        "nvim-treesitter/nvim-treesitter-context",
+                },
+                config = function()
+                        require("nvim-treesitter.configs").setup({
+                                ensure_installed = { "lua", "python" },
+                                highlight = { enable = true },
+                                indent = { enable = true },
+                                textobjects = {
+                                        select = {
+                                                enable = true,
+                                                lookahead = true,
+                                                keymaps = {
+                                                        ["af"] = "@function.outer",
+                                                        ["if"] = "@function.inner",
+                                                        ["ac"] = "@class.outer",
+                                                        ["ic"] = "@class.inner",
+                                                },
+                                        },
+                                },
+                        })
+                end,
+        },      
         { "nvim-lualine/lualine.nvim" },
         { "catppuccin/nvim", name = "catppuccin" },
 

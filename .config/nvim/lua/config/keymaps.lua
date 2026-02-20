e1ddb74252a575864d6daddf60d438ce77a04d3c
Md Mahmud Hasan <mahadymahamudh472@gmail.com>
2026-02-21 05:59:37 +0600

    key binding added

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,14 @@ vim.keymap.set("n", "<leader>bd", ":bd<CR>", {
 
 vim.keymap.set("n", "<Esc>", [[<C-\><C-n>]], { silent = true })
 
+
+vim.keymap.set("n", "<leader>bo", function()
+  local current = vim.api.nvim_get_current_buf()
+
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if buf ~= current and vim.api.nvim_buf_is_loaded(buf) then
+      vim.api.nvim_buf_delete(buf, { force = true })
+    end
+  end
+end, { desc = "Close other buffers" })
+
